5f080c5b8a561e356ec02700088525ecf3452464
Robu - Electronics Store <158466906+Robu-In@users.noreply.github.com>
2024-06-04 17:50:56 +0530

    Added Blink Example

---
+++ Blink Example/Blink.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+pinLED = 4
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(pinLED, GPIO.OUT)
+
+while True:
+        GPIO.output(pinLED, GPIO.HIGH)
+        print("LED on")
+        sleep(1)
+
+        GPIO.output(pinLED, GPIO.LOW)
+        print("LED off")
+        sleep(1)
